5c732781d9ccdc4d91f571074baea3839a35b930
pranjalr004 <pranjalr376@gmail.com>
2026-04-21 23:10:49 +0530

    Total Students enrolled in each course

--- Queries/Join_Query.sql
+++ Queries/Join_Query.sql
@@ -1,2 +1,5 @@
-SELECT s.name,c.name FROM enrollments e JOIN
-students s ON e.student_id=s.id JOIN courses c ON e.course_id=c.id;
+SELECT c.name, s.Name
+FROM enrollments e
+JOIN students s ON e.student_id = s.id
+JOIN courses c ON e.course_id = c.id
+ORDER BY c.name;
